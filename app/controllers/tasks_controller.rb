bde50f5b22c358c1c7a7b87d13f6f72d5d6b21be
APPLE <sandipparida282@gmail.com>
2023-12-13 13:14:29 +0530

    members show in tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,27 @@
 class TasksController < ApplicationController
   before_action :set_team
+  before_action :set_task, only: [:edit, :update]
 
-   def index
+  def index
     user_id = current_user.id
     team_id = @team.id
     team_user= TeamUser.where(user_id: user_id,team_id: team_id)
     @tasks = team_user.first.task_assignment.map { |task_assignment| task_assignment.task }
-    # @tasks = Task.joins(:task_assignments).where(task_assignments: { team_user_id: TeamUser.where(user_id: user_id, team_id: team_id) })
+  end
 
-   end
+  def edit
+    @team_members = @team.members
+  end
+
+  def update
+    if @task.update(task_params)
+      assign_task_to_members(params[:task][:assigned_member_ids])
+      redirect_to team_path(@task.team), notice: 'Task was successfully updated.'
+    else
+      @team_members = @team.members
+      render :edit
+    end
+  end
 
   def new
     @task = @team.tasks.build
@@ -30,9 +43,13 @@ class TasksController < ApplicationController
   private
 
   def set_team
-    @team = Team.includes(:members, :team_users).find(params[:team_id])
+    @team = Team.find(params[:team_id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path, alert: 'Team not found'
+  end
+
+  def set_task
+    @task = @team.tasks.find(params[:id])
   end
 
   def task_params
--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,10 @@
 module TeamsHelper
-end
+    def inlist(lists)
+      colors = %w[bg-red-50 bg-blue-50 bg-green-50 bg-yellow-50 bg-purple-50] # Define different colors
+      lists.map.with_index do |list, index|
+        color_class = colors[index % colors.size] # Cycle through colors based on index
+        content_tag(:span, list, class: "inline-flex items-center rounded-md mx-1 px-2 py-1 text-xs font-medium text-black ring-1 ring-inset #{color_class}")
+      end.join.html_safe
+    end
+  end
+  
